9b1eb4242659a2797c3d49ddf73b4308a4275951
Gabrar <gabriel2025diasss@gmail.com>
2025-12-19 11:18:27 -0300

    Configuração de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import AppRouter from './Routes.jsx'
+import { AuthProvider } from './AuthContext.jsx'
 import './index.css'
-import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
